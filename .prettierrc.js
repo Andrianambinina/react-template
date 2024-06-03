220724e2c3e3fc3577b723d5850fc665791a2f0f
Andrianambinina <31532610+Andrianambinina@users.noreply.github.com>
2024-06-03 18:06:52 +0300

    prettier & eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  bracketSameLine: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  useTabs: false,
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material';
+
+const MuiButton = () => {
+  return <Button variant="contained">Hello world</Button>;
+};
+
+export default MuiButton;
